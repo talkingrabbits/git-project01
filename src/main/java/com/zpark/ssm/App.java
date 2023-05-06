24c37173afe75f306afabf9f76850f15d69905c0
hanlu <hanlu@163.com>
2023-05-06 17:04:51 +0800

    hanlu master forth1 push GitHub!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,13 +3,6 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("hanlu");
-        System.out.println("hanlu111");
-        System.out.println("ha33331111");
-        System.out.println("hanlu111111111111");
-        System.out.println("hanlu2222111");
-        System.out.println("hanlu master first push GitHub!");
-        System.out.println("hanlu master second push GitHub!");
-        System.out.println("hanlu master third push GitHub!");
         System.out.println("hanlu master forth1 push GitHub!");
     }
 }
