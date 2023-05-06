a4b2545bdbb531fae823197d2fcefd489acef1f2
talkingrabbits <107293001+talkingrabbits@users.noreply.github.com>
2023-05-06 17:06:19 +0800

    Update App.java

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -2,13 +2,5 @@ package com.zpark.ssm;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hanlu");
-        System.out.println("hanlu111");
-        System.out.println("ha33331111");
-        System.out.println("hanlu111111111111");
-        System.out.println("hanlu2222111");
-        System.out.println("hanlu master first push GitHub!");
-        System.out.println("hanlu master second push GitHub!");
-        System.out.println("hanlu master third push GitHub!");
     }
 }
