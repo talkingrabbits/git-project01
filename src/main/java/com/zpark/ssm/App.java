5ba32474b6af840f5224d516a5589bf85332bace
hanlu <hanlu@163.com>
2023-05-06 09:44:50 +0800

    first hotfix commint

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hanlu");
         System.out.println("hanlu111");
-        System.out.println("hanlu1111111");
+        System.out.println("hanlu2222111");
     }
 }
